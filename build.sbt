98ba0750dd3538bd35d6cda6cca5164be66a1e2e
Alex Shubert <alex.shubert@gmail.com>
2019-06-02 20:43:10 -0400

    switch to circe'

--- build.sbt
+++ build.sbt
@@ -13,11 +13,12 @@ lazy val root = (project in file("."))
     ),
     libraryDependencies ++= Seq(
       apacheHttp,
-      jackson,
+      jsonParser,
       cats,
       logging,
       logback,
       scalaTest,
+
     )
   )
   .settings(publishSettings)
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,11 +3,9 @@ import sbt._
 //noinspection TypeAnnotation
 object Dependencies {
   val apacheHttp = "org.apache.httpcomponents" % "httpclient" % "4.3.3"
-  val jackson = "com.fasterxml.jackson.core" % "jackson-databind" % "2.9.7"
+  val jsonParser = "io.circe" %% "circe-parser" % "0.11.1"
   val cats = "org.typelevel" %% "cats-core" % "1.4.0"
   val logging = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2"
-
-
   val logback = "ch.qos.logback" % "logback-classic" % "1.2.3" % Test
   val scalaTest = "org.scalatest" %% "scalatest" % "3.0.4" % Test
 }
